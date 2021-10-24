1aa6b51c67f002cc072dabc318f9033e736c5f60
Little Fish <50160052+littlexfish@users.noreply.github.com>
2021-10-24 21:24:00 +0800

    Create main.js

---
+++ main.js
@@ -0,0 +1,25 @@
+
+
+function linkTo(direct) {
+  window.location.href = direct;
+}
+
+
+function getPathAbsolute(path) {
+  return 'https://littlexfish.github.io/Note-Article/' + path;
+}
+
+function linkToRelative(path) {
+  linkTo(getPathAbsolute(path));
+}
+
+
+//render top bar
+
+
+
+
+//render sidebar
+
+
+
